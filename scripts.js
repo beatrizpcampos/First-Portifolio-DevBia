f1c48b195005553ead8dc9cd1bfe5ddd4ff3fefa
beatrizpcampos <beatrizcampos300@gmail.com>
2024-04-13 13:39:12 -0300

    formulario finalizado

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 const form = document.querySelector(".formulario")
+const mascara = document.querySelector(".mascara-formulario")
 
 function activeForm(){
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
 }
+
+function disableForm(){
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+}
+
